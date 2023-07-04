2f56e4969defe00b9d7f6ff6ccb3014d599d2c6a
cryplop <i.g.matsala@gmail.com>
2023-07-04 23:38:28 +0500

    подключение к локальной бд

--- src/main/kotlin/controller/StartController.kt
+++ src/main/kotlin/controller/StartController.kt
@@ -6,12 +6,36 @@ import eu.vendeli.tgbot.annotations.InputHandler
 import eu.vendeli.tgbot.api.message
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.ProcessedUpdate
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
+import java.util.*
+
 
 class StartController {
     @CommandHandler(["/start"])
     suspend fun start(update: ProcessedUpdate, bot: TelegramBot, user: User) {
         message("Привет! Для добавление нового пункта в список желаний, просто напиши мне").send(user, bot)
         bot.inputListener.set(user.id, "addWish")
+        val urls:String = "jdbc:postgresql://localhost/wish"
+        val props = Properties()
+        props.setProperty("user", "postgres")
+        props.setProperty("password", "1")
+        props.setProperty("ssl", "true")
+        val conn: Connection = DriverManager.getConnection(urls, props)
+
+        //val url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true"
+       // val conn: Connection = DriverManager.getConnection(url)
+
+        val st: Statement = conn.createStatement()
+        val rs: ResultSet = st.executeQuery("SELECT * FROM users")
+        while (rs.next()) {
+            print("Column 1 returned ")
+            println(rs.getString(1))
+        }
+        rs.close()
+        st.close()
     }
 
     @InputHandler(["addWish"])
