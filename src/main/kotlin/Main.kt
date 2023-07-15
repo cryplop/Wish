1f10f6d20494d2fd9154065a025cd06eba097b68
cryplop <i.g.matsala@gmail.com>
2023-07-16 00:16:50 +0500

    вывод списка подарков

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,5 @@
-import eu.vendeli.tgbot.TelegramBot
-import kotlinx.coroutines.runBlocking
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.Statement
-import java.util.*
 
+import eu.vendeli.tgbot.TelegramBot
 
 suspend fun main(){
     val bot = TelegramBot("5347737744:AAG3rIQlFpLpmSCEjo6Ge2MoM03DG6njjU4","controller")
--- src/main/kotlin/controller/StartController.kt
+++ src/main/kotlin/controller/StartController.kt
@@ -6,17 +6,28 @@ import eu.vendeli.tgbot.annotations.InputHandler
 import eu.vendeli.tgbot.api.message
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.ProcessedUpdate
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.Statement
-import java.util.*
-import list.wish
+import java.io.File
 
 class StartController {
     @CommandHandler(["/start"])
     suspend fun start(update: ProcessedUpdate, bot: TelegramBot, user: User) {
         message("Привет! Для добавление нового пункта в список желаний, просто напиши мне").send(user, bot)
+        bot.inputListener.set(user.id, "addWish")
+    }
+
+    @CommandHandler(["/wishlist"])
+    suspend fun wishlist(update: ProcessedUpdate, bot: TelegramBot, user: User) {
+        //проверить наличие файла со списком желаний
+        val file = File("wishlist/{${user.username}}.out")
+        //если файл найден, то прочитать его содержимое и вывести пользователю
+        if (file.exists()) {
+            message("Привет! Вот твой список!").send(user, bot)
+            for (wish in File("wishlist/{${user.username}}.out").readLines()) {
+                message(wish).send(user, bot)
+            }
+        } else {
+            message("Список подарков не найден!").send(user, bot)
+        }
         bot.inputListener.set(user.id, "addWish")
     }
 
